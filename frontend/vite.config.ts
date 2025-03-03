65c4e2ed2334ab51be828e7b1caa132978e6cde6
Maximilian Krone <m.krone@fme.de>
2025-03-03 15:41:10 +0100

    fix: add config for dns

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: 3000, host: true}
+  server: {
+    port: 3000,
+    host: true,
+    strictPort: true,
+    allowedHosts: ["your-domain"] }
 })
