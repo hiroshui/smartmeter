c0294999be6c985a429e7f303952614217be5823
Maximilian Krone <m.krone@fme.de>
2025-03-04 11:25:49 +0100

    fix: Brightness7 not used

--- frontend/src/components/ThemeToggle.tsx
+++ frontend/src/components/ThemeToggle.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { IconButton } from "@mui/material";
-import { Brightness4, Brightness7 } from "@mui/icons-material";
+import { Brightness4 } from "@mui/icons-material";
 import { useTheme } from "../ThemeProvider";
 
 const ThemeToggle: React.FC = () => {
